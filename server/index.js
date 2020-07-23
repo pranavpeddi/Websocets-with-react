a7cd0b6035809c63a5b501c12d3154a4fa9d4c68
pranav peddi <41251294+pranavpeddi@users.noreply.github.com>
2020-07-23 17:06:42 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,72 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+//to generate unique id
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+
+const clients = {};
+
+const users = {};
+
+let editorContent = null;
+
+let userActivity = [];
+
+
+
+const sendMessage = (json) => {
+ 
+  Object.keys(clients).map((client) => {
+    clients[client].sendUTF(json);
+  });
+}
+
+const typesDef = {
+  USER_EVENT: "userevent",
+  CONTENT_CHANGE: "contentchange"
+}
+
+wsServer.on('request', function(request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      const dataFromClient = JSON.parse(message.utf8Data);
+      const json = { type: dataFromClient.type };
+      if (dataFromClient.type === typesDef.USER_EVENT) {
+        users[userID] = dataFromClient;
+        userActivity.push(`${dataFromClient.username} joined to edit the document`);
+        json.data = { users, userActivity };
+      } else if (dataFromClient.type === typesDef.CONTENT_CHANGE) {
+        editorContent = dataFromClient.content;
+        json.data = { editorContent, userActivity };
+      }
+      sendMessage(JSON.stringify(json));
+    }
+  });
+  // user disconnected
+  connection.on('close', function(connection) {
+    console.log((new Date()) + " Peer " + userID + " disconnected.");
+    const json = { type: typesDef.USER_EVENT };
+    userActivity.push(`${users[userID].username} left the document`);
+    json.data = { users, userActivity };
+    delete clients[userID];
+    delete users[userID];
+    sendMessage(JSON.stringify(json));
+  });
+});
